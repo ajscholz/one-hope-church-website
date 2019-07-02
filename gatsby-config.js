55a076c8b997dec102477b773e5ebac2d4672b83
Andrew Scholz <yoitsscholzy@gmail.com>
2019-07-02 14:50:09 -0400

    update branding in package.json and gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,18 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    name: `One Hope Community Church`,
+    shortName: `One Hope Church`,
+    description: `A multi-generational, multi-ethnic church in northeast Columbus.`,
+    pastor: `Felipe Ferreira`,
+    address: `1389 E. Cooke Rd.`,
+    city: `Columbus`,
+    state: `OH`,
+    zip: `43224`,
+    phone: "614-294-4717",
+    email: "info@onehopecommunity.church",
+    ministries: {
+      kids: "One Hope Kids",
+    },
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -15,18 +25,18 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
-    },
+    // {
+    //   resolve: `gatsby-plugin-manifest`,
+    //   options: {
+    //     name: `One Hope Church`,
+    //     short_name: `One Hope`,
+    //     start_url: `/`,
+    //     background_color: `#663399`,
+    //     theme_color: `#663399`,
+    //     display: `minimal-ui`,
+    //     icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+    //   },
+    // },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
