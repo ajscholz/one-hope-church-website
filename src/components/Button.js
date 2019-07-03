91d37311fe079923c6af8afbe4121038f667bbb5
Andrew Scholz <yoitsscholzy@gmail.com>
2019-07-02 20:49:31 -0400

    add button component

---
+++ src/components/Button.js
@@ -0,0 +1,9 @@
+import styled from "styled-components"
+
+export default styled.button`
+  text-transform: uppercase;
+  color: white;
+  font-size: 0.9rem;
+  margin: 1em 2em;
+  background-color: orange;
+`
