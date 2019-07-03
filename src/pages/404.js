72862846a6dca489b2ab26b2662d5ab8bfa3c27b
Andrew Scholz <yoitsscholzy@gmail.com>
2019-07-03 01:18:09 -0400

    fix path errors breaking build

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,8 +2,8 @@ import React from "react"
 import { graphql } from "gatsby"
 import styled from "styled-components"
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+import Layout from "../components/Layout"
+import SEO from "../components/Seo"
 import HeroImage from "../components/HeroImage"
 
 export const query = graphql`
