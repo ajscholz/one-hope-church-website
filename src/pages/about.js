cf966bcb9e63e368ada58ed3428a89e41931f321
Andrew Scholz <yoitsscholzy@gmail.com>
2019-07-03 00:05:18 -0400

    add section component to all pages

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,6 +4,7 @@ import { graphql } from "gatsby"
 import Layout from "../components/Layout"
 import SEO from "../components/Seo"
 import HeroImage from "../components/HeroImage"
+import Section from "../components/Section"
 
 export const query = graphql`
   {
@@ -21,8 +22,10 @@ const About = ({ data }) => (
   <Layout>
     <HeroImage image={data.hero.childImageSharp.fluid} />
     <SEO title="About" />
-    <h1>Hi from the About page</h1>
-    <p>Welcome to the about page</p>
+    <Section>
+      <h1>Hi from the About page</h1>
+      <p>Welcome to the about page</p>
+    </Section>
   </Layout>
 )
 
