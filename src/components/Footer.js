a53ccdfc36a3fe3dcd91e002cec2d028d27598d9
Andrew Scholz <yoitsscholzy@gmail.com>
2019-07-03 00:40:08 -0400

    update footer information

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,9 +4,15 @@ import styled from "styled-components"
 export default ({ hide }) => {
   return (
     <Footer hide={hide}>
-      © {new Date().getFullYear()}, Built with
-      {` `}
-      <a href="https://www.gatsbyjs.org">Gatsby</a>
+      © {new Date().getFullYear()}
+      {`, Built with 💙 by `}
+      <a
+        href="https://ajsolutions.netlify.com"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        {` AJSolutions`}
+      </a>
     </Footer>
   )
 }
@@ -21,6 +27,10 @@ const Footer = styled.footer`
   display: ${props => (props.hide ? "none" : "flex")};
   justify-content: center;
   align-items: center;
+  & a:visited,
+  a:active {
+    color: gray;
+  }
   @media (min-width: 577px) {
     margin-bottom: 0;
   }
