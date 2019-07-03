3573f0bb12d61a86a4de033ee4c2f38a1f2c76e4
Andrew Scholz <yoitsscholzy@gmail.com>
2019-07-02 22:10:42 -0400

    make site title a link to homepage

---
+++ src/pages/about.js
@@ -0,0 +1,29 @@
+import React from "react"
+import { graphql } from "gatsby"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import HeroImage from "../components/HeroImage"
+
+export const query = graphql`
+  {
+    hero: file(name: { eq: "about-hero" }) {
+      childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+`
+
+const About = ({ data }) => (
+  <Layout>
+    <HeroImage image={data.hero.childImageSharp.fluid} />
+    <SEO title="About" />
+    <h1>Hi from the About page</h1>
+    <p>Welcome to the about page</p>
+  </Layout>
+)
+
+export default About
