fb376c11038482a5b47df8450d26fbec3b1de95b
Andrew Scholz <yoitsscholzy@gmail.com>
2019-07-02 21:06:37 -0400

    add react-icons

---
+++ src/components/BottomNav.js
@@ -0,0 +1,46 @@
+import React from "react"
+import styled from "styled-components"
+import { links } from "../constants/Links"
+import { Link } from "gatsby"
+
+export default () => {
+  return (
+    <Navbar>
+      <List>
+        {links.map(link => {
+          return (
+            <StyledLink as={Link} to={link.path} key={link}>
+              {link.text}
+            </StyledLink>
+          )
+        })}
+      </List>
+    </Navbar>
+  )
+}
+
+const Navbar = styled.nav`
+  position: fixed;
+  bottom: 0;
+  height: 4rem;
+  width: 100vw;
+  background: white;
+  display: flex;
+  align-items: center;
+`
+
+const List = styled.ul`
+  width: 100%;
+  list-style: none;
+  padding-inline-start: 0;
+  display: flex;
+  justify-content: space-evenly;
+`
+
+const StyledLink = styled.li`
+  font-weight: 300;
+  text-transform: capitalize;
+  font-size: 0.8rem;
+  color: black;
+  padding: 0.5rem;
+`
