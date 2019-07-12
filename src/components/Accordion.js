441940bd5059389fa0f0ad9e07a15ea04b90173e
Andrew Scholz <yoitsscholzy@gmail.com>
2019-07-12 08:20:44 -0400

    add accordion component

---
+++ src/components/Accordion.js
@@ -0,0 +1,91 @@
+import React from "react"
+import styled from "styled-components"
+import {
+  Accordion,
+  AccordionItem,
+  AccordionItemHeading,
+  AccordionItemPanel,
+  AccordionItemButton,
+} from "react-accessible-accordion"
+
+export default ({ questions }) => {
+  return (
+    <StyledAccordion allowZeroExpanded={true}>
+      {questions.map((question, index) => {
+        return (
+          <StyledAccordionItem key={index}>
+            <AccordionItemHeading>
+              <Question>{question.question}</Question>
+            </AccordionItemHeading>
+            <Answer>{question.answer}</Answer>
+          </StyledAccordionItem>
+        )
+      })}
+    </StyledAccordion>
+  )
+}
+
+const StyledAccordion = styled(Accordion)`
+  width: 100%;
+  max-width: 600px;
+`
+
+const StyledAccordionItem = styled(AccordionItem)`
+  width: 100%;
+  height: auto;
+  margin: 0.5rem 0 0.5rem 0;
+  border: 2px solid var(--primaryLight);
+  border-radius: 28px;
+  overflow: hidden;
+  position: relative;
+  background: var(--primaryLight);
+  display: grid;
+  grid-template-columns: 100%;
+  grid-template-rows: auto auto;
+  transition: all 0.3s;
+  ${Question}:hover {
+    background: var(--primary);
+  }
+  ${Question}:focus {
+    background: var(--primary);
+  }
+`
+
+const Question = styled(AccordionItemButton)`
+  padding: 1rem 2rem;
+  background: transparent;
+  display: flex;
+  align-items: center;
+  transition: all 0.3s;
+  z-index: 5;
+  outline: none;
+  &::before {
+    content: "";
+    position: relative;
+    left: -0.25rem;
+    height: 0.5rem;
+    width: 0.5rem;
+    border: 1px solid var(--blue);
+    border-top: none;
+    border-left: none;
+    transform: rotate(-45deg);
+    margin-right: 0.6rem;
+    transition: var(--mainTransition);
+  }
+  &[aria-expanded="true"]::before,
+  &[aria-selected="true"]::before {
+    transform: rotate(45deg);
+  }
+  &:hover {
+    cursor: pointer;
+    border: 2px solid var(--primary);
+  }
+  &:focus {
+    border: 2px solid var(--primary);
+  }
+`
+
+const Answer = styled(AccordionItemPanel)`
+  padding: 1rem 2rem;
+  transition: all 0.3s;
+`
--- src/pages/visit.js
+++ src/pages/visit.js
@@ -8,6 +8,9 @@ import Section from "../components/Section"
 import Banner from "../components/Banner"
 import Title from "../components/Title"
 import ContactForm from "../components/ContactForm"
+import Accordion from "../components/Accordion"
+
+import questions from "../utils/questions"
 
 export const query = graphql`
   {
@@ -52,47 +55,7 @@ const Visit = ({ data }) => (
     </Section>
     <Section>
       <Title>Common Questions</Title>
-      <ul>
-        <li>
-          <h3>What is a service like?</h3>
-          <p>
-            Our gatherings are simple and sacred. Come as you are, connect with
-            others, and enjoy some coffee. Each gathering is about one hour and
-            a half and includes a meaningful message and some great music.
-            Church should be fun, laid back, and authentic.{" "}
-          </p>
-        </li>
-        <li>
-          <h3>What do you have for my kids?</h3>
-          <p>
-            Each Sunday we offer experiences for children from five months
-            through fifth grade that are designed specifically for them.
-          </p>
-          <p>
-            For youth and young adults we have hang outs on Thursdays at 6:30pm.
-          </p>
-        </li>
-        <li>
-          <h3>Who will be there?</h3>
-          <p>
-            We are multicultural and multigenerational. Unity in our diversity
-            makes us stronger and unique. At One Hope, we have people of all
-            ages and all cultures in our gatherings. Currently we have members
-            from Brazil, El Salvador, Mexico, Eritrea, United States, United
-            Kingdom, Jamaica, Guyana!
-          </p>
-        </li>
-        <li>
-          <h3>When should I arrive?</h3>
-          <p>
-            If you are planning to check your children in to Kids at One Hope
-            Kids, you may want to arrive at around 10:15. This will give you
-            plenty of time to register your kids and bring them to their room.
-            If you aren't checking-in any kids, we would recommend arriving a
-            few minutes before 10:30.
-          </p>
-        </li>
-      </ul>
+      <Accordion questions={questions} />
     </Section>
     <Section>
       <Title>Any other questions?</Title>
