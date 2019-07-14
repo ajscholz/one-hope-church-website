361dbf65032487636808898792ed8453d6e07faa
Andrew Scholz <yoitsscholzy@gmail.com>
2019-07-13 21:02:49 -0400

    improve footer style

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,25 +4,41 @@ import styled from "styled-components"
 export default ({ hide }) => {
   return (
     <Footer hide={hide}>
-      © {new Date().getFullYear()}
-      {`, Built with 💙 by `}
-      <a
-        href="https://ajsolutions.netlify.com"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        {` AJSolutions`}
-      </a>
+      <StyledP>
+        © {new Date().getFullYear()}&nbsp;&nbsp;|&nbsp;&nbsp;One Hope Community
+        Church
+      </StyledP>
+      <StyledP>
+        {`Built with 💙 by `}
+        <span>
+          <a
+            href="https://ajsolutions.netlify.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {`AJSolutions`}
+          </a>
+        </span>
+      </StyledP>
     </Footer>
   )
 }
 
+const StyledP = styled.p`
+  margin: 0 auto;
+  &:first-of-type {
+    margin-bottom: 0.35rem;
+  }
+`
+
 const Footer = styled.footer`
+  font-size: 0.9rem;
   background: var(--blue);
   color: white;
   width: 100vw;
-  height: 4rem;
+  padding: 2rem;
   display: ${props => (props.hide ? "none" : "flex")};
+  flex-direction: column;
   justify-content: center;
   align-items: center;
   & a:visited,
