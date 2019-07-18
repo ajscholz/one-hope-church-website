7e36949ef4d1698423dda9aab383012f4746d0cb
Andrew Scholz <yoitsscholzy@gmail.com>
2019-07-18 16:23:55 -0400

    add plugin-offline and manifest back

--- gatsby-config.js
+++ gatsby-config.js
@@ -71,20 +71,20 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
 
-    // {
-    //   resolve: `gatsby-plugin-manifest`,
-    //   options: {
-    //     name: `One Hope Church`,
-    //     short_name: `One Hope`,
-    //     start_url: `/`,
-    //     background_color: `#663399`,
-    //     theme_color: `#663399`,
-    //     display: `minimal-ui`,
-    //     icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-    //   },
-    // },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `One Hope Church`,
+        short_name: `One Hope`,
+        start_url: `/`,
+        background_color: `#ffb348`,
+        theme_color: `#f8820d`,
+        display: `minimal-ui`,
+        icon: `src/images/favicon.png`, // This path is relative to the root of the site.
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-plugin-offline`,
   ],
 }
